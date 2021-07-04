6212c1fdd87db989993bb3d8a80c4811e9440ef7
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-07-04 21:34:47 +0800

    组合求和

--- combine_sum.cpp
+++ combine_sum.cpp
@@ -1,3 +1,8 @@
+// leetcode T39:Combination Sum
+
+// 算法思想：回溯法
+// 参考：https://leetcode-cn.com/problems/combination-sum/solution/zu-he-zong-he-by-leetcode-solution/
+
 class Solution {
 public:
     void dfs(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& combine, int idx) {
@@ -25,8 +30,3 @@ public:
         return ans;
     }
 };
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/combination-sum/solution/zu-he-zong-he-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
