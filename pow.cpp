aab1d357ae8506cb5c0d4429d942c09198be42a5
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-07-21 10:12:48 +0800

    添加方法二 快速幂+迭代法

--- pow.cpp
+++ pow.cpp
@@ -1,7 +1,11 @@
 /* leetcode T50:Pow(求幂)
  * 原题链接：https://leetcode-cn.com/problems/powx-n/
- *
- * 方法一：快速幂+递归
+ * */
+#include <iostream>
+
+using namespace std;
+
+/* 方法一：快速幂+递归
  * 快速幂算法的本质是分治算法。Pow(x,n)
  *
  * 为提高算法计算速度，我们不再采用逐个x相乘的方法，从x 开始，每次直接把上一次的结果进行平方。
@@ -21,9 +25,6 @@
  * ***补充：递归算法的时间复杂度计算问题（参考知乎：https://www.zhihu.com/question/63075755）
  * 递归算法的时间复杂度本质上是要看: 递归的次数 * 每次递归中的操作次数
  * */
-#include <iostream>
-
-using namespace std;
 
 class Solution {
 public:
@@ -52,3 +53,51 @@ public:
             return 1.0 / baseMul(x,n);
     }
 };
+
+
+/* 方法二:快速幂+迭代
+ * 该方法不是特别理解,具体可参考
+ * https://leetcode-cn.com/problems/powx-n/solution/powx-n-by-leetcode-solution/(方法二)
+ *
+ * 复杂度分析:
+ * 时间复杂度:O(log n)
+ * 空间复杂度:O(n)
+ * */
+
+class Solution2 {
+public:
+    double baseMul(double x,long long N) {
+        double ans = 1.0;
+        /*贡献的初始值为x*/
+        double x_contribution = x;
+        /*在对N进行二进制拆分的同时计算答案*/
+        while (N > 0) {
+            if (N % 2 == 1){//奇数
+                /*如果N二进制表示的最低位为1(表示为奇数),那么需计入贡献*/
+                ans *= x_contribution;
+            }
+            /*将贡献不断地平方*/
+            x_contribution *= x_contribution;
+            // 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可(不理解)
+            N /= 2;
+        }
+        return ans;
+    }
+
+    double myPow(double x,int n) {
+        long long N = n;
+        return N >= 0 ? baseMul(x,N) : 1.0 / baseMul(x,N);
+    }
+};
+
+/*测试*/
+int main() {
+    Solution *solution = new Solution();
+
+    Solution2 *solution2 = new Solution2();
+
+    int x = 2;
+    int n = 10;
+
+    cout << x << "^" << n << " is " << solution2->myPow(x,n) << endl;
+}
