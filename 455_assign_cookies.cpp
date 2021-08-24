c01123a25f543e9e3ecb2fade0922a895c66e9a0
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-08-24 08:37:08 +0800

    添加新方法实现

--- 455_assign_cookies.cpp
+++ 455_assign_cookies.cpp
@@ -9,6 +9,10 @@
 /* 方法一：排序 + 贪心
  *
  * 算法思想：
+ * 核心思想：
+ * 为了尽可能满足最多数量的孩子，从贪心的角度考虑，应该按照孩子的胃口从小到大的顺序依次满足每个孩子，
+ * 且对于每个孩子，应该选择可以满足这个孩子的胃口且尺寸最小的饼干。
+ *
  * 因为饥饿度最小的孩子最容易吃饱,所以我们先考虑这个孩子。为了尽量使得剩下的饼干可
  * 以满足饥饿度更大的孩子,所以我们应该把大于等于这个孩子饥饿度的、且大小最小的饼干给这
  * 个孩子。满足了这个孩子之后,我们采取同样的策略,考虑剩下孩子里饥饿度最小的孩子,直到
@@ -19,12 +23,14 @@
  * 这样我们就可以从饥饿度最小的孩子和大小最小的饼干出发,计算有多少个对子可以满足条件。
  *
  * 复杂度分析：
- * 时间复杂度：暂定；
- * 空间复杂度：暂定；
+ * 时间复杂度：O(mlogm+nlogn)，两数组排序的时间复杂度分别为O(mlogm)和O(nlogn)，
+ * 遍历数组的复杂度O(m+n)-->总的时间复杂度为O(mlogm+nlogn)；
+ * 空间复杂度：O(logm+logn)，主要为排序的空间开销；
  * */
 
 using namespace std;
 
+/*算法实现*/
 int findContentChildren(vector<int> & children, vector<int> & cookies) {
     sort(children.begin(), children.end());
     sort(cookies.begin(), cookies.end());
@@ -38,9 +44,34 @@ int findContentChildren(vector<int> & children, vector<int> & cookies) {
     return child;
 }
 
+/*我的实现*/
+int findContentChildren2(vector<int> kids, vector<int> cookies) {
+    sort(kids.begin(),kids.end());
+    sort(cookies.begin(),cookies.end());
+
+    int kids_size = kids.size(), cookies_size = cookies.size();
+    int i = 0, j = 0;
+
+    // 易错点：如果不加&& j < cookies_size，
+    // 当while (j < cookies_size)完成时，i就不再++了，此时while(i<kids_size)就会陷入死循环
+    while (i < kids_size && j < cookies_size) {
+        while (j < cookies_size) {
+            if (cookies[j] >= kids[i]) {
+                i++;
+                j++;// j的下一次循环从当前j的j+1开始，不必从0开始-->整个算法只需要遍历kids_size+cookies_size次
+                break;
+            }
+            j++;
+        }
+    }
+
+    return i;
+}
+
+/*测试*/
 int main() {
     vector<int> children = {1,2};
-    vector<int> cookies = {1,1};
-    int num = findContentChildren(children, cookies);
+    vector<int> cookies = {1,2,3};
+    int num = findContentChildren2(children, cookies);
     printf("num: %d\n",num);
 }
